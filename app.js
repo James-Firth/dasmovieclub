9bff1e444be0f6a1729ee341ed1d1571c8432e64
James Firth <james.firth@norimaconsulting.com>
2017-06-30 14:29:15 -0500

    DB Data is stored in a reasonable spot. Added theme submission and theme viewing
    TODO: Make it look good

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
+const themeController = require('./controllers/theme');
+const themeSubmissionController = require('./controllers/themeSubmission');
 
 /**
  * API keys and Passport configuration.
@@ -129,6 +131,9 @@ app.get('/signup', userController.getSignup);
 app.post('/signup', userController.postSignup);
 app.get('/contact', contactController.getContact);
 app.post('/contact', contactController.postContact);
+app.get('/themes', themeController.getThemes);
+app.get('/themes/submit', themeSubmissionController.getThemeForm);
+app.post('/themes/submit', themeSubmissionController.postThemeForm);
 app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
 app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
 app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
